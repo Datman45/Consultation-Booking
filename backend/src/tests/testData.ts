6f275b94d9992317e733858ef63ef9d026c19b9b
Arseni Bogdanov <arsenibogdanov@gmail.com>
2026-07-07 11:33:26 +0300

    fixing comments

--- backend/src/tests/testData.ts
+++ backend/src/tests/testData.ts
@@ -1,12 +1,13 @@
-// these data used for creating a booking
+// Test data used in createBooking.test.ts.
+// The client must have at least 500 credits.
 export const BookingTestClientId = "2e3b395f-6a71-4591-9f6f-3870e346a45b";
 export const BookingTestExpertId = "0e1512bf-26d2-4384-8598-3bf4c4fc81fa";
 export const BookingTestSlotId = "7d1ea84b-2e96-4c56-a118-3787a31445da";
 
-//these data used in viewBooking.test.ts
+// Test data used in viewBooking.test.ts.
 export const viewBookingTestMissingId = "7a1ea54b-2e76-1c56-a118-3787a31445da";
 
-//these data used in client.test.ts for creating a client
+// Test client credit values used in client.test.ts.
 export const firstClientCredits = 500;
 export const secondClientCredits = 50;
 export const thirdClientCredits = 100;
